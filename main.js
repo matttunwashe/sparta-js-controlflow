8032b38c3f28d9a280e1435140c2361e31c20678
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-10-30 15:34:38 +0000

    if and switch

---
+++ main.js
@@ -0,0 +1,41 @@
+// var money_in_my_pocket = prompt('How much money do you have');
+// var drinks_drunk = prompt('How dman have you had already?');
+// var price_of_crisps = prompt('How much are those crisps');
+//
+//
+// if (money_in_my_pocket > 10 && drinks_drunk < 3) {
+//   console.log('Another drink please!');
+// }
+// else if (money_in_my_pocket > 5 || price_of_crisps == 0) {
+// console.log('Just crisps then');
+// }
+// else {
+// console.log("I'm off home... sigh");
+// }
+//
+
+
+
+// 
+// var name = "tim";
+// if (name === "bob") {
+//   console.log("Hi Bob");
+// } else if (name === "tim") {
+//   console.log("Hi Tim");
+// }
+//
+
+
+switch (name) {
+  case "bob":
+  console.log("Hi Bob");
+    break;
+
+    case "tim":
+    console.log("Hi Tim");
+      break;
+
+  default:
+  console.log("Hi default person");
+
+}
